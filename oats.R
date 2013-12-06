7abcd1243b4b23b669e955bfd2f38b812b5852cd
Dublin R <DublinRUserGroup@gmail.com>
2013-12-06 23:30:19 +0000

    Create oats.R

---
+++ oats.R
@@ -0,0 +1,15 @@
+
+library(MASS)
+data(oats)
+names(oats) = c('block', 'variety', 'nitrogen', 'yield')
+oats$mainplot = oats$variety
+oats$subplot = oats$nitrogen
+ 
+summary(oats)
+library(nlme)
+m1.nlme = lme(yield ~ variety*nitrogen,
+                      random = ~ 1|block/mainplot,
+                      data = oats)
+ 
+summary(m1.nlme)
+anova(m1.nlme)
